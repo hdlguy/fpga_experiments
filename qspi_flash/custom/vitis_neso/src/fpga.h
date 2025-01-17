b8c02730163fc9e3682c8a08c16a399d3a9b9258
hdlguy <hdlguy@hdlguy.com>
2025-01-17 16:10:24 -0500

    adding folder for custom Artix board

---
+++ qspi_flash/custom/vitis_neso/src/fpga.h
@@ -0,0 +1,9 @@
+#define 	REG_BASEADDR 		XPAR_M_AXI_BASEADDR
+
+// define AXI registers
+#define     Nreg                16  // number of 32 bit AXI registers
+
+#define 	FPGA_VERSION		0	// currently 0x07010100
+#define		FPGA_ID				1	// currently 0xdeadbeef
+
+#define		LED_CONTROL			3
