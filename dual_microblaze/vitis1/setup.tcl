84d14659eafd4ef310ab2fcc74a8e778e74a781f
hdlguy <hdlguy@hdlguy.com>
2025-03-03 18:20:26 -0500

    working on vitis for dual microblaze

--- dual_microblaze/vitis1/setup.tcl
+++ dual_microblaze/vitis1/setup.tcl
@@ -21,9 +21,9 @@ platform create -name "standalone_plat" -hw $hw -proc $proc -os standalone
 #file link -symbolic ./workspace/icap_test/src/test.c               ../../../src/icap_test/test.c
 #file link -symbolic ./workspace/icap_test/src/fpga.h               ../../../src/fpga.h
 
-#app create -name hello1 -platform standalone_plat -domain standalone_domain -template "Empty Application(C)"
-#file link -symbolic ./workspace/hello1/src/test.c               ../../../src/hello1/test.c
-#file link -symbolic ./workspace/hello1/src/fpga.h               ../../../src/fpga.h
+app create -name hello1 -platform standalone_plat -domain standalone_domain -template "Empty Application(C)"
+file link -symbolic ./workspace/hello1/src/test.c               ../../../src/hello1/test.c
+file link -symbolic ./workspace/hello1/src/fpga.h               ../../../src/fpga.h
 
 app build all
 
