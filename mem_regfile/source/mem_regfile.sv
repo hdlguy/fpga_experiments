c51315bfd5f5f5f1c9333f02481d7495a89912e6
hdlguy <hdlguy@hdlguy.com>
2025-04-29 07:38:28 -0400

    first interation of mem_regfile.sv complete, ready to verify

--- mem_regfile/source/mem_regfile.sv
+++ mem_regfile/source/mem_regfile.sv
@@ -1,24 +1,44 @@
-
+// This module implements a register file to be controlled by an AXI Bram Controller.
+// The bram controller goes inside the IPI block diagram so that addresses are reported in the XSA file.
+// This module goes out in the Verilog code so that two dimensional arrays can be used in the application interface.
+//
 module mem_regfile #(
-    parameter int Naddr = 4+2,      // number of byte address bits
-    parameter int Nregs = 2**(Naddr-2)
+    parameter int                       Naddr = 4+2,      // number of byte address bits
+    parameter int                       Nregs = 2**(Naddr-2),
+    parameter logic[Nregs-1:0][31:0]    init_reg = 0;
 ) (
+    // interface to bram controller
     input   logic                   clk,
     input   logic[Naddr-1;0]        addr,
     input   logic[31:0]             wr_data,
     output  logic[31:0]             rd_data,
-    input   logic                   enable,
+    input   logic                   en,
     input   logic                   reset,
-    input   logic[3:0]              wr_enable,
-    //
+    input   logic[3:0]              wr_en,
+    // interface to application
     output  logic[Nregs-1:0][31:0]  reg_val,    // current register value
-    output  logic[Nregs-1:0][31:0]  pulse_val,  // single cycle pulse for any bits written as 1. (self-clearing)
+    output  logic[Nregs-1:0][31:0]  pul_val,    // single cycle pulse for any bits written as 1. (self-clearing)
     input   logic[Nregs-1:0][31:0]  read_val    // array of values that can be read back.
 );
 
 
+    output  logic[Nregs-1:0][31:0]  reg_val_int = init_reg;
     always_ff @(posedge clk) begin
+
+        // read
+        if (en) begin
+            rd_data <= read_val[addr];
+        end
+
+        // write
+        if ((wr_en[0]) && (en)) begin reg_val[addr][ 7: 0] <= wr_data[ 7: 0]; pul_val[addr][ 7: 0] <= wr_data[ 7: 0]; end else begin pul_val[addr][ 7: 0] <= 0; end
+        if ((wr_en[1]) && (en)) begin reg_val[addr][15: 8] <= wr_data[15: 8]; pul_val[addr][15: 8] <= wr_data[15: 8]; end else begin pul_val[addr][15: 8] <= 0; end
+        if ((wr_en[2]) && (en)) begin reg_val[addr][23:16] <= wr_data[23:16]; pul_val[addr][23:16] <= wr_data[23:16]; end else begin pul_val[addr][23:16] <= 0; end
+        if ((wr_en[3]) && (en)) begin reg_val[addr][31:24] <= wr_data[31:24]; pul_val[addr][31:24] <= wr_data[31:24]; end else begin pul_val[addr][31:24] <= 0; end
+    
     end
+
+    assign reg_val = reg_val_int;
 
 
 endmodule
