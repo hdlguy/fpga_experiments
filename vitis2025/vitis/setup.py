7ea39cc57e73be64e226536cfaf96f3756f33206
hdlguy <hdlguy@hdlguy.com>
2025-06-10 13:36:56 -0400

    putting minimal source into git

--- vitis2025/vitis/setup.py
+++ vitis2025/vitis/setup.py
@@ -1,19 +1,32 @@
-# 2025-06-10T10:46:10.795407
+# vitis -s setup.py
+# vitis --workspace ./workspace
+
 import vitis
 
 client = vitis.create_client()
-client.set_workspace(path="vitis")
+client.set_workspace(path="workspace")
 
 advanced_options = client.create_advanced_options_dict(dt_overlay="0")
 
-platform = client.create_platform_component(name = "standalone_plat",hw_design = "$COMPONENT_LOCATION/../../implement/results/top.xsa",os = "standalone",cpu = "microblaze_0",domain_name = "standalone_microblaze_0",generate_dtb = False,advanced_options = advanced_options,compiler = "gcc")
+platform = client.create_platform_component(
+    name = "standalone_plat",
+    hw_design = "../implement/results/top.xsa",
+    os = "standalone",
+    cpu = "microblaze_0",
+    domain_name = "standalone_microblaze_0",
+    generate_dtb = False,
+    advanced_options = advanced_options,
+    compiler = "gcc"
+)
 
 platform = client.get_component(name="standalone_plat")
+
 status = platform.build()
 
 comp = client.create_app_component(name="hello_world",platform = "$COMPONENT_LOCATION/../standalone_plat/export/standalone_plat/standalone_plat.xpfm",domain = "standalone_microblaze_0",template = "hello_world")
 
 comp = client.get_component(name="hello_world")
+
 status = comp.clean()
 
 status = platform.build()
---
+++ vitis2025/vitis/src/hello_world/helloworld.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+//#include "platform.h"
+#include "xil_printf.h"
+
+
+int main()
+{
+    //init_platform();
+
+    print("Hello World\n\r");
+    print("Successfully ran Hello World application\n\r");
+
+    //cleanup_platform();
+    
+    return(0);
+    
+}
