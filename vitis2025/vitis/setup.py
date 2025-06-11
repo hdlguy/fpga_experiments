0fdd52ab1d62710a5375643d217116b30e12ccf9
hdlguy <hdlguy@hdlguy.com>
2025-06-11 18:17:41 -0400

    removed commented python from setup.py

--- vitis2025/vitis/setup.py
+++ vitis2025/vitis/setup.py
@@ -35,16 +35,11 @@ comp = client.create_app_component(
     name=app_name,
     platform = "$COMPONENT_LOCATION/../standalone_plat/export/standalone_plat/standalone_plat.xpfm",
     domain = "standalone_microblaze_0",
-    #template = "hello_world"
     template = "empty_application"
 )
 
-#comp.import_files("./src/hello_world/hello_world.c")
-
 os.system('ln -s ../../../src/hello_world/hello_world.c workspace/hello_world/src/hello_world.c')
 os.system('ln -s ../../../src/fpga.h workspace/hello_world/src/fpga.h')
-
-#comp = client.get_component(name=app_name)
 
 status = comp.clean()
 status = platform.build()
