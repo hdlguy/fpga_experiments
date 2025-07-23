2fc0058f27644d17927067d81dc631fc437d3c1e
hdlguy <hdlguy@hdlguy.com>
2025-07-23 08:34:57 -0400

    mem_regfile tested in hello1, working

--- video_generation/vitis/src/fpga.h
+++ video_generation/vitis/src/fpga.h
@@ -4,6 +4,6 @@
 #define 	FPGA_VERSION		0	// currently 0x07010100
 #define		FPGA_ID				1	// currently 0xdeadbeef
 
-#define		FPGA_RGB_LED		2
+//#define		FPGA_RGB_LED		2
 // [2:0] = LED1 [blue, green, red]
 // [6:4] = LED2 [blue, green, red]
--- video_generation/vitis/src/hello1/test.c
+++ video_generation/vitis/src/hello1/test.c
@@ -1,7 +1,7 @@
 #include "xil_printf.h"
 #include "xparameters.h"
 #include "xv_tpg_hw.h"
-//#include "fpga.h"
+#include "fpga.h"
 
 
 int main()
@@ -9,9 +9,8 @@ int main()
 
     xil_printf("Hello World\n\r");
     
-    // uint32_t *regptr = (uint32_t *)XPAR_M00_AXI_BASEADDR;
-    //uint32_t *regptr = (uint32_t *)XPAR_AXI_REGFILE_CTRL_BASEADDR;
-    //xil_printf("regptr = %p\n\r", regptr);
+    uint32_t *regptr = (uint32_t *)XPAR_REGFILE_CTRL_BASEADDR;
+    xil_printf("regptr = %p\n\r", regptr);
 
     uint32_t *tpgptr = (uint32_t *)XPAR_V_TPG_0_BASEADDR;
     xil_printf("tpgptr = %p\n\r", tpgptr);
@@ -25,7 +24,7 @@ int main()
     uint32_t whilecount=0;
 	while(1){
 
-        //xil_printf("%08d: FPGA_ID = 0x%08x, FPGA_VERSION = 0x%08x\n\r", whilecount, regptr[FPGA_ID], regptr[FPGA_VERSION]);
+        xil_printf("%08d: FPGA_ID = 0x%08x, FPGA_VERSION = 0x%08x\n\r", whilecount, regptr[FPGA_ID], regptr[FPGA_VERSION]);
         xil_printf("%08d: hello\n\r", whilecount);
 
         for(int i=0; i<100000000; i++);
