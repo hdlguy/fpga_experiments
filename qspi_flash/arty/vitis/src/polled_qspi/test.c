8092c9ea381be1065c41fbea59a6feb0c44d6426
hdlguy <hdlguy@hdlguy.com>
2025-09-09 07:35:05 -0400

    polled_qspi example project

--- qspi_flash/arty/vitis/src/polled_qspi/test.c
+++ qspi_flash/arty/vitis/src/polled_qspi/test.c
@@ -8,9 +8,7 @@
 
 typedef u8 DataBuffer[BUFFER_SIZE];
 
-
 int SpiPolledExample(XSpi *SpiInstancePtr, UINTPTR BaseAddress);
-
 
 static XSpi  SpiInstance;	 /* The instance of the SPI device */
 
@@ -20,7 +18,6 @@ u8 WriteBuffer[BUFFER_SIZE];
 int main(void)
 {
 	int Status;
-
 
 	Status = SpiPolledExample(&SpiInstance, XPAR_XSPI_0_BASEADDR);
 
@@ -35,10 +32,8 @@ int main(void)
 
 
 int SpiPolledExample(XSpi *SpiInstancePtr, UINTPTR BaseAddress)
-
 {
 	int Status;
-	u32 Count;
 	u8 Test;
 	XSpi_Config *ConfigPtr;	/* Pointer to Configuration data */
 
@@ -64,7 +59,7 @@ int SpiPolledExample(XSpi *SpiInstancePtr, UINTPTR BaseAddress)
 	// 	return XST_SUCCESS;
 	// }
 
-	Status = XSpi_SetOptions(SpiInstancePtr, XSP_MASTER_OPTION | XSP_LOOPBACK_OPTION);
+	Status = XSpi_SetOptions(SpiInstancePtr, XSP_MASTER_OPTION);
 	if (Status != XST_SUCCESS) {
 		xil_printf("XSpi_SetOptions error\n\r");
 		return XST_FAILURE;
@@ -76,7 +71,7 @@ int SpiPolledExample(XSpi *SpiInstancePtr, UINTPTR BaseAddress)
 	// XSpi_IntrGlobalDisable(SpiInstancePtr);
 
 	Test = 0x10;
-	for (Count = 0; Count < BUFFER_SIZE; Count++) {
+	for (int Count = 0; Count < BUFFER_SIZE; Count++) {
 		WriteBuffer[Count] = (u8)(Count + Test);
 		ReadBuffer[Count] = 0;
 	}
@@ -84,7 +79,7 @@ int SpiPolledExample(XSpi *SpiInstancePtr, UINTPTR BaseAddress)
 
 	XSpi_Transfer(SpiInstancePtr, WriteBuffer, ReadBuffer, BUFFER_SIZE);
 
-	for (Count = 0; Count < BUFFER_SIZE; Count++) {
+	for (int Count = 0; Count < BUFFER_SIZE; Count++) {
 		if (WriteBuffer[Count] != ReadBuffer[Count]) {
 			xil_printf("XSpi_Transfer error\n\r");
 			return XST_FAILURE;
