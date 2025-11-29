f80d0698ad55a2722bacf726e793774373a2d6d6
Your Name <you@example.com>
2025-11-29 09:54:01 -0500

    wdt test runs but I don't understand usage

--- wdt_test/vitis/src/wdt_test/test.c
+++ wdt_test/vitis/src/wdt_test/test.c
@@ -21,14 +21,14 @@ int main()
     uint32_t whilecount=0;
     while(1) {
 
-      uint32_t rval = wdt_ptr[XWT_TWCSR0_OFFSET];
+      uint32_t rval = wdt_ptr[XWT_TWCSR0_OFFSET/4];
       uint32_t reset_status = (rval >> 3) & 0x01;
       uint32_t timer_state  = (rval >> 2) & 0x01;
 
 		  xil_printf("0x%08x: ", whilecount);
       xil_printf("reset_status = %d, timer_state = %d\n\r", reset_status, timer_state);
 
-      wdt_ptr[XWT_TWCSR0_OFFSET] = 0x0c; // clear the reset status and timer state.
+      wdt_ptr[XWT_TWCSR0_OFFSET/4] = 0x0c; // clear the reset status and timer state.
 
       // xil_printf("XWT_TWCSR0_OFFSET = 0x%08x\n\r", wdt_ptr[XWT_TWCSR0_OFFSET/4]);
       // xil_printf("XWT_TBR_OFFSET    = 0x%08x\n\r", wdt_ptr[XWT_TBR_OFFSET/4]);
