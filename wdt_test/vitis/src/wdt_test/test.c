3df13d1f2c7893362387e4e0732fc7613bb7357b
hdlguy <hdlguy@hdlguy.com>
2025-12-01 09:33:30 -0500

    wdt_test cleanup

--- wdt_test/vitis/src/wdt_test/test.c
+++ wdt_test/vitis/src/wdt_test/test.c
@@ -1,13 +1,9 @@
+// This bare-metal is C code to experiment with the AXI Watchdog Timer.
 #include <stdlib.h>
 #include "xil_printf.h"
 #include "xparameters.h"
 #include "xstatus.h"
-// #include "xwdttb_l.h"
 #include "xwdttb_hw.h"
-
-
-
-
 
 int main()
 {
@@ -16,27 +12,28 @@ int main()
 
     xil_printf("\n\rHello World\n\r");
 
-    wdt_ptr[XWT_TWCSR1_OFFSET/4] = 1; // enable the wdt 
+    // enable the wdt - comment this to disable the WDT
+    wdt_ptr[XWT_TWCSR1_OFFSET/4] = 1; 
 
     uint32_t whilecount=0;
     while(1) {
 
-      uint32_t rval = wdt_ptr[XWT_TWCSR0_OFFSET];
-      uint32_t reset_status = (rval >> 3) & 0x01;
-      uint32_t timer_state  = (rval >> 2) & 0x01;
+        uint32_t rval = wdt_ptr[XWT_TWCSR0_OFFSET];
+        uint32_t reset_status = (rval >> 3) & 0x01;
+        uint32_t timer_state  = (rval >> 2) & 0x01;
 
-		  xil_printf("0x%08x: ", whilecount);
-      xil_printf("reset_status = %d, timer_state = %d\n\r", reset_status, timer_state);
+        xil_printf("0x%08x: ", whilecount);
+        xil_printf("reset_status = %d, timer_state = %d\n\r", reset_status, timer_state);
 
-      wdt_ptr[XWT_TWCSR0_OFFSET] = 0x0c; // clear the reset status and timer state.
+        wdt_ptr[XWT_TWCSR0_OFFSET] = 0x0c; // clear the reset status and timer state.
 
-      // xil_printf("XWT_TWCSR0_OFFSET = 0x%08x\n\r", wdt_ptr[XWT_TWCSR0_OFFSET/4]);
-      // xil_printf("XWT_TBR_OFFSET    = 0x%08x\n\r", wdt_ptr[XWT_TBR_OFFSET/4]);
+        // xil_printf("XWT_TWCSR0_OFFSET = 0x%08x\n\r", wdt_ptr[XWT_TWCSR0_OFFSET/4]);
+        // xil_printf("XWT_TBR_OFFSET    = 0x%08x\n\r", wdt_ptr[XWT_TBR_OFFSET/4]);
 
-    	for(int i=0; i<6000000; i++); // delay
-    	whilecount++;
-
+        for(int i=0; i<6000000; i++); // delay
+        whilecount++;
     }
     
     return 0;
 }
+
