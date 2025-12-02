691ffb648ba597ab6b66ae2396a51d15030a65d4
hdlguy <hdlguy@hdlguy.com>
2025-12-02 07:28:25 -0500

    comment cleanup

--- wdt_test/vitis/src/wdt_test/test.c
+++ wdt_test/vitis/src/wdt_test/test.c
@@ -11,10 +11,11 @@
 
 int main()
 {
+    // get pointers to WDT and GPIO
     uint32_t* wdt_ptr = (uint32_t *) XPAR_XWDTTB_0_BASEADDR;
     uint32_t* gpio_ptr = (uint32_t *) XPAR_XGPIO_0_BASEADDR;
 
-    xil_printf("\n\rHello World\n\r");
+    xil_printf("\n\rHello World I'm coming out of reset!\n\r");
 
     // enable the wdt 
     wdt_ptr[XWT_TWCSR1_OFFSET/4] = 1;
@@ -22,24 +23,28 @@ int main()
     uint32_t whilecount=0;
     while(1) {
 
+        // extract and print WDT status bits
         uint32_t rval = wdt_ptr[XWT_TWCSR0_OFFSET/4];
         uint32_t reset_status = (rval >> 3) & 0x01;
         uint32_t timer_state  = (rval >> 2) & 0x01;
-
         xil_printf("0x%08x: ", whilecount);
         xil_printf("reset_status = %d, timer_state = %d\n\r", reset_status, timer_state);
 
-        wdt_ptr[XWT_TWCSR0_OFFSET/4] = 0x0c; // clear the reset status and timer state.
-        gpio_ptr[XGPIO_DATA_OFFSET/4] = 0x00ff & whilecount;
+        // clear the reset status and timer state before the WDT expires.
+        wdt_ptr[XWT_TWCSR0_OFFSET/4] = 0x0c;                    
 
-        // xil_printf("XWT_TWCSR0_OFFSET = 0x%08x\n\r", wdt_ptr[XWT_TWCSR0_OFFSET/4]);
-        // xil_printf("XWT_TBR_OFFSET    = 0x%08x\n\r", wdt_ptr[XWT_TBR_OFFSET/4]);
+        // increment the LEDs
+        gpio_ptr[XGPIO_DATA_OFFSET/4] = 0x00ff & whilecount;    
 
-        while (gpio_ptr[XGPIO_DATA2_OFFSET/4] != 0) {}; // simulate a fault by waiting while a button is pressed
+        // simulate a fault by waiting while a button is pressed
+        while (gpio_ptr[XGPIO_DATA2_OFFSET/4] != 0) {}; 
 
-        for(int i=0; i<4000000; i++); // delay
+        // delay for about 1 second
+        for(int i=0; i<4000000; i++); 
+
         whilecount++;
     }
     
     return 0;
 }
+
