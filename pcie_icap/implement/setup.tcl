d6b2ae2fb48058863346388296601df618008db8
pedro <pete@hdlguy.com>
2026-06-05 09:53:30 -0600

    pcie_icap fpga compilable

--- pcie_icap/implement/setup.tcl
+++ pcie_icap/implement/setup.tcl
@@ -7,47 +7,17 @@ set_property target_language Verilog [current_project]
 set_property default_lib work [current_project]
 load_features ipintegrator
 
-#read_ip ../source/vinstru/source/vinstru_ila/vinstru_ila.xci
-#read_ip ../source/top_ila/top_ila.xci
-#upgrade_ip -quiet  [get_ips *]
-#generate_target {all} [get_ips *]
-
-source ../source/vinstru/source/vinstru_ila.tcl
 source ../source/top_ila.tcl
 
 source ../source/system.tcl
 generate_target {synthesis implementation} [get_files ./proj.srcs/sources_1/bd/system/system.bd]
 set_property synth_checkpoint_mode None    [get_files ./proj.srcs/sources_1/bd/system/system.bd]
 
-source ../source/system2.tcl
-generate_target {synthesis implementation} [get_files ./proj.srcs/sources_1/bd/system2/system2.bd]
-set_property synth_checkpoint_mode None    [get_files ./proj.srcs/sources_1/bd/system2/system2.bd]
-
-read_verilog -sv ../source/iir_verilog/iir_filter.sv
-read_verilog -sv ../source/iir_verilog/iir_sos_dsp48.sv
-read_verilog -sv ../source/iir_verilog/round_n_sat.sv
-read_verilog -sv ../source/iir_verilog/iir_mult_accum.sv
-
-read_verilog -sv ../source/gng/rtl/gng_coef.v  
-read_verilog -sv ../source/gng/rtl/gng_ctg.v  
-read_verilog -sv ../source/gng/rtl/gng_interp.v  
-read_verilog -sv ../source/gng/rtl/gng_lzd.v  
-read_verilog -sv ../source/gng/rtl/gng_smul_16_18_sadd_37.v  
-read_verilog -sv ../source/gng/rtl/gng_smul_16_18.v  
-read_verilog -sv ../source/gng/rtl/gng.v
-
-read_verilog -sv ../source/vinstru/source/vsource.sv  
-read_verilog -sv ../source/vinstru/source/vinstru.sv  
-
 read_verilog -sv ../source/mem_regfile/mem_regfile.sv
 
 read_verilog -sv ../source/top.sv
 
 read_xdc ../source/top.xdc
-
-add_files -norecurse ../vitis/release/production.elf
-set_property SCOPED_TO_REF system2 [get_files -all -of_objects [get_fileset sources_1] {production.elf}]
-set_property SCOPED_TO_CELLS { microblaze_0 } [get_files -all -of_objects [get_fileset sources_1] {production.elf}]
 
 close_project
 
--- pcie_icap/source/top.sv
+++ pcie_icap/source/top.sv
@@ -1,7 +1,6 @@
-// 0x00000102   - added system2 block diagram to handle qspi access.
 
 module top #(
-    parameter logic[31:0] VERSION       = 32'h00000102,
+    parameter logic[31:0] VERSION       = 32'h00000101,
     parameter logic[31:0] ID            = 32'hdeadbeef
 )(
     //
@@ -17,10 +16,7 @@ module top #(
     output  logic[0:0]  pcie_mgt_txn,
     output  logic[0:0]  pcie_mgt_txp,
     input   logic       pcie_reset,
-    output  logic       pcie_clkreq_l,
-    // QSPI to config flash, sclk not shown because dedicated CCLK pin is used.
-    inout   logic[3:0]  qspi_data,
-    inout   logic       qspi_ss
+    output  logic       pcie_clkreq_l
 );
 
     assign pcie_clkreq_l = 1'b0;
@@ -35,22 +31,6 @@ module top #(
 
     logic axi_aclk, axi_aresetn;
 
-    logic [13:0]    vinstru_bram_addr;
-    logic           vinstru_bram_clk;
-    logic [31:0]    vinstru_bram_din;
-    logic [31:0]    vinstru_bram_dout;
-    logic           vinstru_bram_en;
-    logic           vinstru_bram_rst;
-    logic [3:0]     vinstru_bram_we;    
-    
-    logic [31:0]    flash_bram_addr;
-    logic           flash_bram_clk;
-    logic [31:0]    flash_bram_din;
-    logic [31:0]    flash_bram_dout;
-    logic           flash_bram_en;
-    logic           flash_bram_rst;
-    logic [3:0]     flash_bram_we;
-        
     logic[3:0] led;
     
     // block diagram containing PCIe interface
@@ -72,74 +52,10 @@ module top #(
         .regfile_we         (regfile_we),  
         //
         .axi_aclk           (axi_aclk),
-        .axi_aresetn        (axi_aresetn),
-        //
-        .vinstru_bram_addr  (vinstru_bram_addr),
-        .vinstru_bram_clk   (vinstru_bram_clk),
-        .vinstru_bram_din   (vinstru_bram_din),
-        .vinstru_bram_dout  (vinstru_bram_dout),
-        .vinstru_bram_en    (vinstru_bram_en),
-        .vinstru_bram_rst   (vinstru_bram_rst),
-        .vinstru_bram_we    (vinstru_bram_we),
-        //
-        .flash_bram_addr    (flash_bram_addr),
-        .flash_bram_clk     (flash_bram_clk),
-        .flash_bram_din     (flash_bram_din),
-        .flash_bram_dout    (flash_bram_dout),
-        .flash_bram_en      (flash_bram_en),
-        .flash_bram_rst     (flash_bram_rst),
-        .flash_bram_we      (flash_bram_we)                                     
+        .axi_aresetn        (axi_aresetn)
     );
         
-    logic[3:0] qspi_io_i, qspi_io_o, qspi_io_t; 
-    logic qspi_ss_i, qspi_ss_o, qspi_ss_t;       
     logic startup_cfgclk, startup_cfgmclk, startup_eos, startup_preq;
-    
-    // block diagram containing Microblaze for QSPI flash control.
-    system2 system2_i (
-        .resetn                 (axi_aresetn),
-        .clkin                  (axi_aclk),
-        //
-        .led_tri_o              (led[3]),
-        //
-        .qspi_io0_i             (qspi_io_i[0]),
-        .qspi_io0_o             (qspi_io_o[0]),
-        .qspi_io0_t             (qspi_io_t[0]),
-        .qspi_io1_i             (qspi_io_i[1]),
-        .qspi_io1_o             (qspi_io_o[1]),
-        .qspi_io1_t             (qspi_io_t[1]),
-        .qspi_io2_i             (qspi_io_i[2]),
-        .qspi_io2_o             (qspi_io_o[2]),
-        .qspi_io2_t             (qspi_io_t[2]),
-        .qspi_io3_i             (qspi_io_i[3]),
-        .qspi_io3_o             (qspi_io_o[3]),
-        .qspi_io3_t             (qspi_io_t[3]),
-        .qspi_ss_i              (qspi_ss_i),
-        .qspi_ss_o              (qspi_ss_o),
-        .qspi_ss_t              (qspi_ss_t),
-        //
-        .startup_cfgclk         (startup_cfgclk),
-        .startup_cfgmclk        (startup_cfgmclk),
-        .startup_preq           (startup_preq),
-        //
-        .flash_bram_addr        (flash_bram_addr),
-        .flash_bram_clk         (flash_bram_clk),
-        .flash_bram_din         (flash_bram_din),
-        .flash_bram_dout        (flash_bram_dout),
-        .flash_bram_en          (flash_bram_en),
-        .flash_bram_rst         (flash_bram_rst),
-        .flash_bram_we          (flash_bram_we)        
-    );
-
-    IOBUF qspi_io0_iobuf (.I(qspi_io_o[0]), .IO(qspi_data[0]), .O(qspi_io_i[0]), .T(qspi_io_t[0]));
-    IOBUF qspi_io1_iobuf (.I(qspi_io_o[1]), .IO(qspi_data[1]), .O(qspi_io_i[1]), .T(qspi_io_t[1]));
-    IOBUF qspi_io2_iobuf (.I(qspi_io_o[2]), .IO(qspi_data[2]), .O(qspi_io_i[2]), .T(qspi_io_t[2]));
-    IOBUF qspi_io3_iobuf (.I(qspi_io_o[3]), .IO(qspi_data[3]), .O(qspi_io_i[3]), .T(qspi_io_t[3]));
-    IOBUF qspi_ss_iobuf  (.I(qspi_ss_o),    .IO(qspi_ss),      .O(qspi_ss_i),    .T(qspi_ss_t));
-                    
-
-//    logic clk;
-//    IBUFDS #(.DIFF_TERM("TRUE"), .IBUF_LOW_PWR("TRUE"), .IOSTANDARD("DEFAULT")) IBUFDS_inst (.O(clk), .I(sysclk_p), .IB(sysclk_n));
     
     logic[27:0] led_count;
     always_ff @(posedge axi_aclk) begin
@@ -148,7 +64,7 @@ module top #(
         end else begin
             led_count <= led_count + 1;
         end
-        //led <= led_count[27:24];
+        led[3] <= led_count[27];
     end    
     assign ledn = ~led;
     
@@ -167,27 +83,12 @@ module top #(
     assign led[2:0] = slv_reg[2][2:0];
     assign slv_read[2] = slv_reg[2];
 
-    assign vinstru_pulse_enable = slv_reg[3][0];
     assign slv_read[ 3] = slv_reg[ 3];
-    
-    assign vinstru_run = slv_reg[4][0];
-    assign slv_read[4][3:0] = slv_reg[4][3:0];
-    assign slv_read[4][4] = vinstru_done;
-    assign vinstru_reset = slv_reg[4][8];
-    assign slv_read[4][31:5] = slv_reg[4][31:5];
-    
-    assign vinstru_pulse_period = slv_reg[5];
-    assign slv_read[5] = slv_reg[5];
-    
-    assign vinstru_pulse_width = slv_reg[6][15:0];
-    assign slv_read[6] = slv_reg[6];
-
-    assign vinstru_pulse_amplitude = slv_reg[7][15:0];    
-    assign slv_read[7] = slv_reg[7];
-
-    assign vinstru_noise_amplitude = slv_reg[8][15:0];     
-    assign slv_read[8] = slv_reg[8];
-    
+    assign slv_read[ 4] = slv_reg[ 4];
+    assign slv_read[ 5] = slv_reg[ 5];
+    assign slv_read[ 6] = slv_reg[ 6];
+    assign slv_read[ 7] = slv_reg[ 7];
+    assign slv_read[ 8] = slv_reg[ 8];
     assign slv_read[ 9] = slv_reg[ 9];
     assign slv_read[10] = slv_reg[10];
     assign slv_read[11] = slv_reg[11];
@@ -213,29 +114,9 @@ module top #(
     );
 
 
-    // a virtual instrument
-    vinstru vinstru_inst (
-        .clk                (axi_aclk),
-        .enable             (vinstru_pulse_enable),
-        .reset              (vinstru_reset),
-        .run                (vinstru_run),
-        .done               (vinstru_done),
-        .pulse_period       (vinstru_pulse_period),
-        .pulse_width        (vinstru_pulse_width),
-        .pulse_amplitude    (vinstru_pulse_amplitude),
-        .noise_amplitude    (vinstru_noise_amplitude),
-        //
-        .bram_clk           (vinstru_bram_clk),
-        .bram_rst           (vinstru_bram_rst),
-        .bram_en            (vinstru_bram_en),
-        .bram_we            (vinstru_bram_we),
-        .bram_addr          (vinstru_bram_addr),
-        .bram_din           (vinstru_bram_din),
-        .bram_dout          (vinstru_bram_dout)
-    );
     
     // debug
-    top_ila top_ila_inst(.clk(axi_aclk), .probe0({qspi_io_i, qspi_io_o, qspi_io_t, qspi_ss_i, qspi_ss_o, qspi_ss_t, startup_cfgclk})); //16
+    //top_ila top_ila_inst(.clk(axi_aclk), .probe0({qspi_io_i, qspi_io_o, qspi_io_t, qspi_ss_i, qspi_ss_o, qspi_ss_t, startup_cfgclk})); //16
 
 endmodule
 
