996882f6730981aa862168d168ebe508cd7806b4
hdlguy <hdlguy@hdlguy.com>
2026-07-02 16:47:54 -0400

    lowlevel app is working

--- uartlite_low_level/vitis/src/lowlevel/test.c
+++ uartlite_low_level/vitis/src/lowlevel/test.c
@@ -4,80 +4,73 @@
 #include "xuartlite_l.h"
 #include "xil_printf.h"
 #include "fpga.h"
+#include <sleep.h>
 
-#ifndef SDT
-#define UARTLITE_BASEADDR	   XPAR_UARTLITE_0_BASEADDR
-#else
-#define UARTLITE_BASEADDR	   XPAR_XUARTLITE_0_BASEADDR
-#endif
-
-#define TEST_BUFFER_SIZE 16
+#define TEST_BUFFER_SIZE 10
 
 
-#ifndef SDT
-int UartLiteLowLevelExample(u32 UartliteBaseAddress);
-#else
-int UartLiteLowLevelExample(UINTPTR BaseAddress);
-#endif
 u8 SendBuffer[TEST_BUFFER_SIZE]; /* Buffer for Transmitting Data */
 u8 RecvBuffer[TEST_BUFFER_SIZE]; /* Buffer for Receiving Data */
 
-
-int main(void)
-{
-	int Status;
-
-	/*
-	 * Run the UartLite Low level example, specify the BaseAddress that is
-	 * generated in xparameters.h.
-	 */
-	Status = UartLiteLowLevelExample(UARTLITE_BASEADDR);
-	if (Status != XST_SUCCESS) {
-		xil_printf("Uartlite lowlevel Example Failed\r\n");
-		return XST_FAILURE;
-	}
-
-	xil_printf("Successfully ran Uartlite lowlevel Example\r\n");
-	return XST_SUCCESS;
+void uart_sendchar(uint32_t* uartptr, uint8_t data) {
+	// wait for tx fifo not full
+	while((uartptr[XUL_STATUS_REG_OFFSET/4] & 0x08) != 0);
+	// write to tx fifo
+	uartptr[XUL_TX_FIFO_OFFSET/4] = (uint32_t) data;
 }
 
+int uart_receivechar(uint32_t* uartptr, uint8_t* dptr) {
+	int rxcount=0;
+	while (((uartptr[XUL_STATUS_REG_OFFSET/4])&0x01) != 0) {		
+		dptr[rxcount] = uartptr[XUL_RX_FIFO_OFFSET/4];
+		rxcount++;
+	}
 
-#ifndef SDT
-int UartLiteLowLevelExample(u32 UartliteBaseAddress)
-#else
-int UartLiteLowLevelExample(UINTPTR UartliteBaseAddress)
-#endif
+	return(rxcount);
+}
+
+int main()
 {
-	int Index;
+	int Index, numrx;
 	uint32_t* uartptr = (uint32_t *)XPAR_AXI_UARTLITE_0_BASEADDR; 
-	uint32_t* regptr = (uint32_t *)XPAR_AXI_UARTLITE_0_BASEADDR; 
+	uint32_t* regptr  = (uint32_t *)XPAR_REGFILE_CTRL_BASEADDR; 
 
-	/*
-	 * Initialize the send buffer bytes with a pattern to send and the
-	 * the receive buffer bytes to zero.
-	 */
+	// initialize buffers
 	for (Index = 0; Index < TEST_BUFFER_SIZE; Index++) {
-		SendBuffer[Index] = Index + 'B';
+		SendBuffer[Index] = Index + '0';
 		RecvBuffer[Index] = 0;
 	}
 
 
+	// reset the uart fifos
+	uartptr[XUL_CONTROL_REG_OFFSET/4] = 0x03;
+
+	// send text
+	uart_sendchar(uartptr, '\n'); uart_sendchar(uartptr, '\r');
 	for (Index = 0; Index < TEST_BUFFER_SIZE; Index++) {
-		XUartLite_SendByte(UartliteBaseAddress, SendBuffer[Index]);
+		uart_sendchar(uartptr, SendBuffer[Index]);
 	}
 
-
-	XUartLite_SendByte(UartliteBaseAddress, '\n');
-	XUartLite_SendByte(UartliteBaseAddress, '\r');
-	uint8_t rxchar;
 	while(1) {
-		if (((uartptr[XUL_STATUS_REG_OFFSET])&0x01) != 0) {
-			rxchar = uartptr[XUL_RX_FIFO_OFFSET];
-			uartptr[XUL_TX_FIFO_OFFSET] = rxchar;
-		}
-		
-		regptr[FPGA_LED_CONTROL] += 1;
+		numrx = uart_receivechar(uartptr, RecvBuffer);
+		regptr[FPGA_LED_CONTROL] += numrx;
+
+		uart_sendchar(uartptr, 'H'); uart_sendchar(uartptr, 'Q'); uart_sendchar(uartptr, '\n'); uart_sendchar(uartptr, '\r');
+
+		usleep(1000000);		
 	}
+
+	// // Check for a character and send it out
+	// uart_sendchar(uartptr, '\n'); uart_sendchar(uartptr, '\r');
+	// uint8_t rxchar;
+	// while(1) {
+		
+	// 	if (((uartptr[XUL_STATUS_REG_OFFSET/4])&0x01) != 0) {
+	// 		rxchar = uartptr[XUL_RX_FIFO_OFFSET];
+	// 		uart_sendchar(uartptr, rxchar);
+	// 		regptr[FPGA_LED_CONTROL] += 1;
+	// 	}		
+	// }
 
 	return XST_SUCCESS;
 }
