b838867ce900ba590fc3b279e82a643712dfd675
hdlguy <hdlguy@hdlguy.com>
2026-07-07 14:50:00 -0400

    fit_intc_test working as expected. LEDs incremented in handler.

--- uartlite_low_level/vitis/src/fit_intc_test/test.c
+++ uartlite_low_level/vitis/src/fit_intc_test/test.c
@@ -9,13 +9,11 @@
 
 
 #define INTC_BASEADDR		XPAR_XINTC_0_BASEADDR
-// #define INTC_DEVICE_INTR_ID	0x0U
-// #define INTC_DEVICE_INT_MASK	0x1U
+// interrupt 1 is the FIT
 #define INTC_DEVICE_INTR_ID		0x1U
 #define INTC_DEVICE_INT_MASK	0x2U
 
 
-int IntcLowLevelExample(u32 IntcBaseAddress);
 void SetupInterruptSystem();
 void DeviceDriverHandler(void *CallbackRef);
 
@@ -27,82 +25,43 @@ static XIntc_Config *CfgPtr;
 int main(void)
 {
 	UINTPTR vector_base;
-	u8 Id;
 	u32 IntcBaseAddress = INTC_BASEADDR;
 
 	uint32_t *regptr = (uint32_t *)XPAR_REGFILE_CTRL_BASEADDR;
 
 	xil_printf("\n\r*** running fit_intc_test ***\n\r");
+	xil_printf("FPGA_ID = 0x%08x, FPGA_VERSION = 0x%08x\n\r", regptr[FPGA_ID], regptr[FPGA_VERSION]);
 
-	/*
-	 * Connect a device driver handler that will be called when an interrupt
-	 * for the device occurs, the device driver handler performs the
-	 * specific interrupt processing for the device.
-	 */
+	// Connect a device driver handler 
 	XIntc_RegisterHandler(IntcBaseAddress, INTC_DEVICE_INTR_ID, (XInterruptHandler)DeviceDriverHandler, (void *)0);
 
-	/*
-	 * Enable interrupts for all devices that cause interrupts, and enable
-	 * the INTC master enable bit.
-	 */
+	// Enable interrupts for all devices that cause interrupts, and enable the INTC master enable bit.
 	XIntc_EnableIntr(IntcBaseAddress, INTC_DEVICE_INT_MASK);
 
-
-	/* Set the master enable bit. Note that we do not enable hardware
-	 * interrupts yet since we want to simulate an interrupt from software
-	 * down below. 	 */
-	// XIntc_Out32(IntcBaseAddress + XIN_MER_OFFSET, XIN_INT_MASTER_ENABLE_MASK);
+	// Set the master enable bit and enable hardware interrupts
 	XIntc_Out32(IntcBaseAddress + XIN_MER_OFFSET, XIN_INT_MASTER_ENABLE_MASK|XIN_INT_HARDWARE_ENABLE_MASK);
 
-
-
-
     vector_base = 0x10;
+	//vector_base = XIL_EXCEPTION_ID_INT;
 
-
+	// write the interrupt vector table
 	CfgPtr = LookupConfigByBaseAddress(IntcBaseAddress);
-	if (CfgPtr->VectorAddrWidth > XINTC_STANDARD_VECTOR_ADDRESS_WIDTH) {
-		for (Id = 0; Id < 32 ; Id++) {
-			XIntc_Out64(IntcBaseAddress + XIN_IVEAR_OFFSET + (Id * 8), vector_base);
-		}
-	} else {
-		for (Id = 0; Id < 32 ; Id++) {
-			XIntc_Out32(IntcBaseAddress + XIN_IVAR_OFFSET + (Id * 4), vector_base);
-		}
+	for (u8 Id = 0; Id < 32 ; Id++) {
+		XIntc_Out32(IntcBaseAddress + XIN_IVAR_OFFSET + (Id * 4), vector_base);
 	}
 
-
-	/*
-	 * This step is processor specific, connect the handler for the
-	 * interrupt controller to the interrupt source for the processor.
-	 */
+	/* This step is processor specific, connect the handler for the interrupt controller to the interrupt source for the processor.*/
 	SetupInterruptSystem();
 
-	// /* Cause (simulate) an interrupt so the handler will be called. This is
-	//  * done by writing a 1 to the interrupt status bit for the device
-	//  * interrupt. */
-	// XIntc_Out32(IntcBaseAddress + XIN_ISR_OFFSET, INTC_DEVICE_INT_MASK);
-
-
-	/*
-	 * Wait for the interrupt to be processed, if the interrupt does not
-	 * occur this loop will wait forever.
-	 */
 	uint32_t intcount=0;
 	while (1) {
-		/*
-		 * If the interrupt occurred which is indicated by the global
-		 * variable which is set in the device driver handler, then
-		 * stop waiting.
-		 */
+
 		if (InterruptProcessed) {
-			InterruptProcessed = 0;
+			InterruptProcessed = FALSE;
 			xil_printf("intcount = %u\n\r", intcount);
 			intcount++;
-			regptr[FPGA_LED_CONTROL]++;
 		}
 	}
-
 
 	return XST_SUCCESS;
 
@@ -125,16 +84,12 @@ int main(void)
 void SetupInterruptSystem()
 {
 
-	/*
-	 * Initialize the exception table.
-	 */
+	// Initialize the exception table.
 	Xil_ExceptionInit();
 
-
-	// * Register the interrupt controller handler with the exception table.
+	// Register the interrupt controller handler with the exception table.
 	if (XGet_IntcType(CfgPtr->IntrParent) == XINTC_TYPE_IS_INTC) {
 		Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT, (Xil_ExceptionHandler)XIntc_DeviceInterruptHandler, (void *)INTC_BASEADDR);
-	} else {
 	}
 
 	Xil_ExceptionEnable();
@@ -169,4 +124,7 @@ void DeviceDriverHandler(void *CallbackRef)
 {
 	(void)CallbackRef;
 	InterruptProcessed = TRUE;
+
+	uint32_t *regptr = (uint32_t *)XPAR_REGFILE_CTRL_BASEADDR;
+	regptr[FPGA_LED_CONTROL]++;
 }
